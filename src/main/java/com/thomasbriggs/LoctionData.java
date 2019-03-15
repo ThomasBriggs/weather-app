4054b457f93a789c6824898f6c7b093fb43f536f
Thomas Briggs <thomasbriggs@Thomass-MacBook-Pro.local>
2019-03-15 01:41:23 +0000

    Added JSON class files

---
+++ src/main/java/com/thomasbriggs/LoctionData.java
@@ -0,0 +1,17 @@
+package com.thomasbriggs;
+
+class LocationData
+{
+    String ip;
+    String latitude;
+    String longitude;
+    String zip;
+    String city;
+    Location location;
+}
+
+class Location
+{
+    String capital;
+    String country_flag_emoji;
+}
